9b7133cec70a99e40f5fd1d31ba600e034411e77
Veronika <veronika.korolkova99@gmail.com>
2024-06-17 20:31:08 +0700

    chore: add url DB

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
+
 @Module({
   imports: [
-    // MongooseModule.forRoot(``)
+    MongooseModule.forRoot(`mongodb+srv://${process.env.MONGO_LOGIN}:${process.env.MONGO_PASSWORD}@cluster0.5roqj5k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
   ],
   controllers: [],
   providers: [],
