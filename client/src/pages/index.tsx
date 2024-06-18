b7a40a8c03662f56089b4ef4d053f9867d03c3ec
Chudoyakov Roman <chudoyakov.roman@gmail.com>
2024-06-18 13:55:52 +0700

    chore: updated imports

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,10 +1,10 @@
-import { EnumRoutesName, ICustomRouteProps, routes } from "../shared/lib/routes"
-import { MainPage } from "./main"
+import { EnumRoutesName, ICustomRouteProps, routes } from 'shared/lib/routes';
+import { MainPage } from './main';
 
 export const pages: Record<EnumRoutesName, ICustomRouteProps> = {
   [EnumRoutesName.MAIN]: {
     path: routes.main,
     element: <MainPage />,
-    isAuth: true
+    isAuth: true,
   },
-}
+};
