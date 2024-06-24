f6f4b6dedd0821e236363e78e2bbb7ca21c3bd53
Chudoyakov Roman <chudoyakov.roman@gmail.com>
2024-06-24 18:49:41 +0700

    chore: import env, created public api from screens

--- mobile/src/screens/main.tsx
+++ mobile/src/screens/main.tsx
@@ -1,10 +1,13 @@
+import { TextStyles } from '@/shared/libs/textStyles';
+import { Button } from '@ui-kitten/components';
 import React from 'react';
 import { Text, View } from 'react-native';
 
 export const MainPage = () => {
   return (
     <View>
-      <Text>test123</Text>
+      <Text style={TextStyles.h1}>test</Text>
+      <Button>Text</Button>
     </View>
   );
 };
