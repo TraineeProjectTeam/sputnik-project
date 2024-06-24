de8fcbe36a2a0c6402c6d6c563f513338fea4e53
Chudoyakov Roman <chudoyakov.roman@gmail.com>
2024-06-24 18:47:32 +0700

    feature: created colors enums and typography

---
+++ mobile/src/shared/libs/textStyles.ts
@@ -0,0 +1,68 @@
+import { Colors } from './colors';
+
+export type FontWeight =
+  | 'normal'
+  | 'bold'
+  | '100'
+  | '200'
+  | '300'
+  | '400'
+  | '500'
+  | '600'
+  | '700'
+  | '800'
+  | '900';
+
+export type FontFamily = 'Montserrat-Regular' | 'Montserrat-Medium' | 'Montserrat-Bold';
+
+interface ITextStyles {
+  h1: TextStyle;
+  h2: TextStyle;
+  h3: TextStyle;
+  h4: TextStyle;
+  h5: TextStyle;
+  h6: TextStyle;
+  label: TextStyle;
+  body: TextStyle;
+  bodyBold: TextStyle;
+  button: TextStyle;
+}
+
+class TextStyle {
+  constructor(
+    fontSize: number,
+    fontWeight: FontWeight,
+    color: Colors,
+    fontFamily: FontFamily,
+    lineHeight: number,
+  ) {
+    this.fontSize = fontSize;
+    this.fontWeight = fontWeight;
+    this.color = color;
+    this.fontFamily = fontFamily;
+    this.lineHeight = lineHeight;
+  }
+
+  fontSize: number;
+  fontWeight: FontWeight;
+  color: Colors;
+  fontFamily: FontFamily;
+  lineHeight: number;
+
+  changeColor(color: Colors): TextStyle {
+    return new TextStyle(this.fontSize, this.fontWeight, color, this.fontFamily, this.lineHeight);
+  }
+}
+
+export const TextStyles: ITextStyles = {
+  h1: new TextStyle(36, '600', Colors.Basic800, 'Montserrat-Bold', 36),
+  h2: new TextStyle(32, '600', Colors.Basic800, 'Montserrat-Bold', 32),
+  h3: new TextStyle(28, '600', Colors.Basic800, 'Montserrat-Bold', 28),
+  h4: new TextStyle(24, '600', Colors.Basic800, 'Montserrat-Bold', 24),
+  h5: new TextStyle(20, '600', Colors.Basic800, 'Montserrat-Bold', 20),
+  h6: new TextStyle(16, '600', Colors.Basic800, 'Montserrat-Bold', 16),
+  label: new TextStyle(12, '400', Colors.Basic800, 'Montserrat-Regular', 12),
+  body: new TextStyle(14, '400', Colors.Basic800, 'Montserrat-Regular', 14),
+  bodyBold: new TextStyle(14, '600', Colors.Basic800, 'Montserrat-Bold', 14),
+  button: new TextStyle(14, '600', Colors.Basic800, 'Montserrat-Bold', 14),
+};
