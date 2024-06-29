50eadc212dbdb2a02d0ab3190389f0f67b3d7190
Veronika <veronika.korolkova99@gmail.com>
2024-06-30 01:29:02 +0700

    [feature] Prescribed routes for pages.

--- client/src/app/router/model/routes.ts
+++ client/src/app/router/model/routes.ts
@@ -2,10 +2,14 @@ export enum EnumRoutesName {
   MAIN = 'main',
   PROFILE_CUSTOMER = 'profile_customer',
   PROFILE_VENDOR = 'profile_vendor',
+  LOGIN = 'login',
+  REGISTER = 'register',
 }
 
 export const routes: Record<EnumRoutesName, string> = {
   [EnumRoutesName.MAIN]: '/',
   [EnumRoutesName.PROFILE_CUSTOMER]: '/profile-customer',
   [EnumRoutesName.PROFILE_VENDOR]: '/profile-vendor',
+  [EnumRoutesName.LOGIN]: '/login',
+  [EnumRoutesName.REGISTER]: '/register',
 };
