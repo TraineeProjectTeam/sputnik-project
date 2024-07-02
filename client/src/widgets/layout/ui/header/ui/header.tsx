9253d9f5963c7e42a0e0f21124b821de24e8e49d
Veronika <veronika.korolkova99@gmail.com>
2024-07-02 19:19:55 +0700

    [feature] Added an exit button, working with the header.

--- client/src/widgets/layout/ui/header/ui/header.tsx
+++ client/src/widgets/layout/ui/header/ui/header.tsx
@@ -1,24 +1,40 @@
-import { Layout } from "antd";
-import { routes } from "app/router";
-import { LanguageSelector } from "entities/language-selector";
-import { useTranslation } from "react-i18next";
-import { Link } from "react-router-dom";
-import styled from "styled-components";
+import { Layout } from 'antd';
+import { routes } from 'app/router';
+import { LanguageSelector } from 'entities/language-selector';
+import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { LogoutButton } from 'shared/ui/logout-button/logout-button';
+import useLoginStore from 'features/login-forms/model/login.store';
 
 export const Header = () => {
-  const { t } = useTranslation(['user', 'common'])
-
+  const [tCommon] = useTranslation('common');
+  const [tUser] = useTranslation('user');
+  const isLogin = useLoginStore((state) => state.isLogin);
+  const role = useLoginStore((state) => state.role);
   return (
     <StyledHeader>
       <Link to={routes.main}>Main</Link>
-      <Link to={routes.profile_customer}>{t('Профиль покупателя')}</Link>
-      <Link to={routes.profile_vendor}>{t('Профиль продавца')}</Link>
+      {isLogin ? (
+        <>
+          {role === 'Customer' && (
+            <Link to={routes.profile_customer}>{tUser('Профиль покупателя')}</Link>
+          )}
+          {role === 'Vendor' && <Link to={routes.profile_vendor}>{tUser('Профиль продавца')}</Link>}
+          <LogoutButton />
+        </>
+      ) : (
+        <>
+          <Link to={routes.login}>{tCommon('Войти')}</Link>
+          <Link to={routes.register}>{tCommon('Зарегистрироваться')}</Link>
+        </>
+      )}
       <div>
         <LanguageSelector />
       </div>
     </StyledHeader>
-  )
-}
+  );
+};
 
 const StyledHeader = styled(Layout.Header)`
   height: 3.75rem;
@@ -29,4 +45,5 @@ const StyledHeader = styled(Layout.Header)`
   top: 0;
   width: 100%;
   z-index: 100;
+  align-items: center;
 `;
