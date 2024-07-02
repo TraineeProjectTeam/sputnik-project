e7fcc8b4b85fde11e3be7590daa7ded228354b8d
Veronika <veronika.korolkova99@gmail.com>
2024-07-02 19:20:53 +0700

    [refactor] Changing the location of a function in a structure.

--- client/src/features/login-forms/api/login.api.ts
+++
@@ -1,7 +0,0 @@
-import Cookies from 'js-cookie';
-import { setToken } from 'shared/api/base';
-
-export const saveAccessToken = (token: string) => {
-  Cookies.set('access_token', token, { expires: 7 });
-  setToken();
-};
