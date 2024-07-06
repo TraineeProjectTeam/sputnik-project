d7168d3408c17f98ea02dce9fa01d7858f306c93
Andrey <dimitriev01@list.ru>
2024-07-06 17:52:17 +0300

    fix: layout, added varialbes heights

--- client/src/widgets/layout/ui/footer/ui/footer.tsx
+++ client/src/widgets/layout/ui/footer/ui/footer.tsx
@@ -1,12 +1,12 @@
-import { Layout } from "antd";
-import styled from "styled-components";
+import { Layout } from 'antd';
+import styled from 'styled-components';
 
 export const Footer = () => {
-  return (
-    <StyledFooter>Footer</StyledFooter>
-  )
-}
+  return <StyledFooter>Footer</StyledFooter>;
+};
 
 const StyledFooter = styled(Layout.Footer)`
   background-color: var(--main-background-color);
+  z-index: var(--footer-z-index);
+  height: var(--footer-height);
 `;
--- client/src/widgets/layout/ui/layout.tsx
+++ client/src/widgets/layout/ui/layout.tsx
@@ -11,24 +11,38 @@ export const LayoutComponent = (props: ILayoutProps) => {
     <StyledLayoutConainer>
       <StyledLayout>
         <Header />
-        <StyledContent>{children}</StyledContent>
-        <Footer />
+        <StyledContainer>
+          <StyledContent>{children}</StyledContent>
+          <Footer />
+        </StyledContainer>
       </StyledLayout>
     </StyledLayoutConainer>
-  )
-}
+  );
+};
 
 const StyledLayoutConainer = styled.div`
   display: flex;
   justify-content: center;
   height: 100%;
-`
+`;
 
 const StyledLayout = styled(Layout)`
   max-width: 75rem;
   position: relative;
-`
+  overflow: hidden;
+`;
+
+const StyledContainer = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  overflow-y: auto;
+  overflow-x: hidden;
+`;
 
 const StyledContent = styled(Layout.Content)`
   flex-grow: 1;
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
