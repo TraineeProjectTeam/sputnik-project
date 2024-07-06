b3b3af1418a97596291cbd3381ea8e4fd49a36e7
Chudoyakov Roman <chudoyakov.roman@gmail.com>
2024-07-06 18:13:41 +0700

    refactor: mode user roles to shared/utils

--- mobile/src/entities/user/index.ts
+++ mobile/src/entities/user/index.ts
@@ -1,3 +1,2 @@
 export { useUserStore } from './model/useUserStore';
-export { userRoles } from './model/userRoles';
 export type { IUserWithToken } from './model/useUserStore';
--- mobile/src/shared/utils/userRoles.ts
+++ mobile/src/shared/utils/userRoles.ts
@@ -1,13 +1,12 @@
-import i18n from 'i18next';
 import { Role } from '@/shared/libs/types';
 
 export const userRoles = [
   {
     value: Role.CUSTOMER,
-    label: i18n.t('Role.Покупатель'),
+    label: 'Role.Покупатель',
   },
   {
     value: Role.VENDOR,
-    label: i18n.t('Role.Продавец'),
+    label: 'Role.Продавец',
   },
 ];
