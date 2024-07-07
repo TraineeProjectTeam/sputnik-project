70449f424063ce0c39fedf4e709afa7a581faac9
Chudoyakov Roman <chudoyakov.roman@gmail.com>
2024-07-07 18:37:37 +0700

    feature: created headers

---
+++ mobile/src/shared/ui/header.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Layout, Text, TopNavigation } from '@ui-kitten/components';
+import { useTranslation } from 'react-i18next';
+
+import { TextStyles } from '../libs/textStyles';
+
+export const HeaderBack = ({ title }: { title: string }) => {
+  const { t } = useTranslation();
+
+  return (
+    <Layout>
+      <TopNavigation
+        alignment="center"
+        title={(evaProps) => (
+          <Text {...evaProps} style={TextStyles.h6}>
+            {t(title)}
+          </Text>
+        )}
+      />
+    </Layout>
+  );
+};
---
+++ mobile/src/shared/ui/headerBack.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Layout, Text, TopNavigation, TopNavigationAction } from '@ui-kitten/components';
+import { TouchableWebElement } from '@ui-kitten/components/devsupport';
+import { useTranslation } from 'react-i18next';
+
+import { ArrowLeft } from '../libs/icons';
+import { Colors } from '../libs/colors';
+import { TextStyles } from '../libs/textStyles';
+import { useAppNavigation } from '../libs/useAppNavigation';
+
+export const HeaderBack = ({ title }: { title: string }) => {
+  const navigation = useAppNavigation();
+  const { t } = useTranslation();
+
+  const navigateBack = () => {
+    navigation.goBack();
+  };
+
+  const renderBackAction = (): TouchableWebElement => (
+    <TopNavigationAction
+      onPress={navigateBack}
+      icon={<ArrowLeft size={30} color={Colors.Basic600} />}
+    />
+  );
+
+  return (
+    <Layout>
+      <TopNavigation
+        alignment="center"
+        title={(evaProps) => (
+          <Text {...evaProps} style={TextStyles.h6}>
+            {t(title)}
+          </Text>
+        )}
+        accessoryLeft={renderBackAction}
+      />
+    </Layout>
+  );
+};
