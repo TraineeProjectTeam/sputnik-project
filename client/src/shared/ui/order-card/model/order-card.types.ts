8b60261cd8259b04232370cfe93179feee8fcbae
Andrey <dao.betdevel@gmail.com>
2024-07-18 11:01:53 +0700

    feature: added statuses for order card

--- client/src/shared/ui/order-card/model/order-card.types.ts
+++ client/src/shared/ui/order-card/model/order-card.types.ts
@@ -1,5 +1,24 @@
 import { IOrder } from 'entities/order';
+import { TFunction } from 'i18next';
 
 export interface IOrderCardProps {
   order: IOrder;
+}
+
+export type EnumStatusType = keyof typeof EnumStatus;
+
+export enum EnumStatus {
+  active = 'Активный',
+  inWay = 'В пути',
+  delivered = 'Доставлен',
+  recieved = 'Получен',
+  cancelled = 'Отменен',
+}
+
+export interface IRenderDeliveryDateProps {
+  delivery_date: Date | null;
+  estimated_delivery_date: Date | null;
+  lang: string;
+  tOrder: TFunction<'order', undefined>;
+  status: string;
 }
--- client/src/shared/ui/order-card/ui/order-card.tsx
+++ client/src/shared/ui/order-card/ui/order-card.tsx
@@ -1,10 +1,37 @@
 import { Button, Card } from 'antd';
-import { IOrderCardProps } from '../model/order-card.types';
+import {
+  EnumStatus,
+  EnumStatusType,
+  IOrderCardProps,
+  IRenderDeliveryDateProps,
+} from '../model/order-card.types';
 import { useTranslation } from 'react-i18next';
 import { useCurrentLanguage } from 'shared/lib';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { EnumRoutesName } from 'shared/config';
+
+const renderDeliveryDate = (props: IRenderDeliveryDateProps) => {
+  const { delivery_date, estimated_delivery_date, lang, tOrder } = props;
+
+  if (delivery_date) {
+    return (
+      <p>
+        {tOrder('Дата доставки:', {
+          date: `${new Date(delivery_date).toLocaleTimeString(lang, { day: 'numeric', month: 'long', hour: '2-digit', minute: '2-digit' })}`,
+        })}
+      </p>
+    );
+  } else if (estimated_delivery_date) {
+    return (
+      <p>
+        {tOrder('Предполагаемая дата доставки:', {
+          date: `${new Date(estimated_delivery_date).toLocaleDateString(lang, { day: 'numeric', month: 'long' })}`,
+        })}
+      </p>
+    );
+  }
+};
 
 export const OrderCard = (props: IOrderCardProps) => {
   const { t: tOrder } = useTranslation('order');
@@ -26,7 +53,7 @@ export const OrderCard = (props: IOrderCardProps) => {
                 <StyledLink type="link">{order._id}</StyledLink>
               </div>
               <p>
-                {order.status !== 'cancelled' && (
+                {order.status !== EnumStatus.cancelled && (
                   <StyledPaidSpan>{tOrder('Оплачено')}</StyledPaidSpan>
                 )}{' '}
                 <StyledPrice> {`${order.price} ₽`}</StyledPrice>
@@ -37,27 +64,17 @@ export const OrderCard = (props: IOrderCardProps) => {
         <StyledContent>
           <StyledStatus>
             {tOrder('Доставка в пункт выдачи')}
-            <Button size="small" style={{ cursor: 'default' }} disabled>
-              {tOrder(order.status).charAt(0).toUpperCase() + tOrder(order.status).slice(1)}
+            <Button size="small" disabled>
+              {tOrder(EnumStatus[order.status as EnumStatusType])}
             </Button>
           </StyledStatus>
-          {order.delivery_date ? (
-            <p>
-              {tOrder('Дата доставки:', {
-                date: `${new Date(order.delivery_date).toLocaleTimeString(lang, { day: 'numeric', month: 'long', hour: '2-digit', minute: '2-digit' })}`,
-              })}
-            </p>
-          ) : order.estimated_delivery_date ? (
-            <p>
-              {tOrder('Предполагаемая дата доставки:', {
-                date: `${new Date(order.estimated_delivery_date).toLocaleDateString(lang, { day: 'numeric', month: 'long' })}`,
-              })}
-            </p>
-          ) : (
-            ''
-          )}
-          {/* <p>Pickup Points: {order.pickup_point.join(', ')}</p> */}
-          {/* <p>Products: {order.products.join(', ')}</p> */}
+          {renderDeliveryDate({
+            delivery_date: order.delivery_date,
+            estimated_delivery_date: order.estimated_delivery_date,
+            lang,
+            tOrder,
+            status: order.status,
+          })}
         </StyledContent>
       </StyledCard>
     </Link>
@@ -83,6 +100,9 @@ const StyledPaidSpan = styled.span`
   text-transform: lowercase;
   font-size: 0.875rem;
   font-weight: 400;
+  & > button {
+    cursor: default;
+  }
 `;
 
 const StyledPrice = styled.span`
