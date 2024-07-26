5fad3830f59bbb0a6331db8b55e16a171ce25135
Chudoyakov Roman <chudoyakov.roman@gmail.com>
2024-07-26 19:36:34 +0700

    refactor: changed styles to add   to cart button

--- mobile/src/features/AddToCartBtn/ui/AddToCartBtn.tsx
+++ mobile/src/features/AddToCartBtn/ui/AddToCartBtn.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
 import { useTranslation } from 'react-i18next';
 import { Button, Text } from '@ui-kitten/components';
 
@@ -14,11 +13,7 @@ interface AddToCartBtnProps {
 export const AddToCartBtn: React.FC<AddToCartBtnProps> = ({ size = 'medium' }) => {
   const { t } = useTranslation();
   return (
-    <Button
-      size={size}
-      style={styles.button}
-      accessoryLeft={<ShoppingBag size={16} color={Colors.Basic100} />}
-    >
+    <Button size={size} accessoryLeft={<ShoppingBag size={16} color={Colors.Basic100} />}>
       {(evaProps) => (
         <Text {...evaProps} style={TextStyles.button.changeColor(Colors.Basic100)}>
           {t('Product.В корзину')}
@@ -27,13 +22,3 @@ export const AddToCartBtn: React.FC<AddToCartBtnProps> = ({ size = 'medium' }) =
     </Button>
   );
 };
-
-const styles = StyleSheet.create({
-  button: {
-    marginLeft: 7,
-    marginRight: 7,
-    padding: 0,
-    marginTop: 'auto',
-    marginBottom: 7,
-  },
-});
