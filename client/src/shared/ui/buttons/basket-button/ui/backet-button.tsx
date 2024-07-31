fae29c550f0b89bbc4196e16a69080446b5ce152
Andrey Dimitriev <dimitriev01@list.ru>
2024-07-31 11:55:05 +0700

    refactor: hide add product to baket

--- client/src/shared/ui/buttons/basket-button/ui/backet-button.tsx
+++ client/src/shared/ui/buttons/basket-button/ui/backet-button.tsx
@@ -1,12 +1,18 @@
 import { Button } from 'antd';
+import useLoginStore from 'features/login-forms/model/login.store';
 import { useTranslation } from 'react-i18next';
 
 export const BasketButton = () => {
   const { t } = useTranslation('product');
+  const { role } = useLoginStore();
 
   const handleAddToCart = () => {
     console.log('Item added to cart');
   };
+
+  if (role !== 'Customer') {
+    return null;
+  }
 
   return (
     <Button type="primary" onClick={handleAddToCart}>
