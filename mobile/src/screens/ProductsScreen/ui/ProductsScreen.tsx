8441e3dfcf20b7321a7db7e8866bc6d1a4aa53a8
Chudoyakov Roman <chudoyakov.roman@gmail.com>
2024-08-06 22:43:14 +0700

    fix: when auth set set cart and favorites,

--- mobile/src/screens/ProductsScreen/ui/ProductsScreen.tsx
+++ mobile/src/screens/ProductsScreen/ui/ProductsScreen.tsx
@@ -1,22 +1,33 @@
 import React, { useEffect } from 'react';
 
 import { ProductList } from '@/widgets/ProductList';
+
 import { useUserFormStore } from '@/features/user-form';
+
+import { useCartStore } from '@/entities/Cart';
 import { useProductStore } from '@/entities/Product';
-import { PageSpinner } from '@/shared/ui/PageSpinner';
 import { useFavoriteStore } from '@/entities/Favorite';
+
+import { PageSpinner } from '@/shared/ui/PageSpinner';
+import { storage } from '@/shared/libs/storage';
 
 export const ProductsScreen = () => {
   const { products, getProducts, isLoading } = useProductStore();
   const { getUser } = useUserFormStore();
   const { setFavoriteIds } = useFavoriteStore();
+  const { setProductsIds } = useCartStore();
+
+  const isAuth = storage.contains('access_token');
 
   useEffect(() => {
-    const getData = async () => {
-      const data = await getUser();
-      setFavoriteIds(data.featured);
-    };
-    getData();
+    if (isAuth) {
+      const getData = async () => {
+        const data = await getUser();
+        setFavoriteIds(data.featured);
+        setProductsIds(data.cart);
+      };
+      getData();
+    }
     getProducts();
   }, []);
 
