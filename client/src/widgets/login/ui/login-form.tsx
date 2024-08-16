ff4d79582ea401c362f97badc6128dd91de0b72f
Andrey Dimitriev <dimitriev01@list.ru>
2024-08-16 16:10:12 +0700

    refactor: validation login page for password

--- client/src/widgets/login/ui/login-form.tsx
+++ client/src/widgets/login/ui/login-form.tsx
@@ -84,7 +84,7 @@ export const LoginForm = (props: ILoginForm) => {
           <Input type="tel" placeholder="+12345678901" />
         </Form.Item>
       )}
-      <Form.Item label={t('Пароль')} name="password">
+      <Form.Item label={t('Пароль')} name="password" rules={rulesForFormItems(t).password}>
         <Input.Password autoComplete="on" />
       </Form.Item>
       <Form.Item name="role" label={t('Войти как')}>
