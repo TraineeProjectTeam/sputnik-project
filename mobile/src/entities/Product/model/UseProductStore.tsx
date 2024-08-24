5d8c808f59de4e02e986e5a7b60f7110d66632b1
Chudoyakov Roman <chudoyakov.roman@gmail.com>
2024-08-24 17:46:44 +0700

    feature: immer in product store

--- mobile/src/entities/Product/model/UseProductStore.tsx
+++ mobile/src/entities/Product/model/UseProductStore.tsx
@@ -1,12 +1,13 @@
 import { createWithEqualityFn } from 'zustand/traditional';
 import { ICharacteristic, IFilters, IProduct } from '@/shared/libs/types';
 import { getCategoriesRequest, getCharacteristicsRequest, getProductsRequest } from '../api/api';
+import { immer } from 'zustand/middleware/immer';
 
 interface IProductStore {
   products: IProduct[];
   categories: string[];
   filters: IFilters;
-  cachedCategory: string;
+  cachedCategory?: string;
   cachedFilters: IFilters;
   characteristics: ICharacteristic[];
   currentPage: number;
@@ -37,126 +38,140 @@ const initialCachedFiltersState: IFilters = {
   max: 0,
 };
 
-export const useProductStore = createWithEqualityFn<IProductStore>((set, get) => ({
-  products: [],
-  categories: [],
-  filters: initialFiltersState,
-  cachedCategory: '',
-  cachedFilters: initialCachedFiltersState,
-  characteristics: [],
-  currentPage: 0,
-  totalPages: 0,
-  isLoading: true,
-  isLoadingChar: false,
-  prices: {
-    max: 0,
-    min: 0,
-  },
-  getCategories: async () => {
-    try {
-      set({ isLoading: true });
-      const data = await getCategoriesRequest();
-      set({ categories: data });
-      return data;
-    } catch {
-      throw new Error();
-    } finally {
-      set({ isLoading: false });
-    }
-  },
-  getProducts: async (filters) => {
-    try {
-      set((state) => ({
-        isLoading: true,
-        currentPage: 1,
-        filters: {
+export const useProductStore = createWithEqualityFn<IProductStore>()(
+  immer((set, get) => ({
+    products: [],
+    categories: [],
+    filters: initialFiltersState,
+    cachedCategory: '',
+    cachedFilters: initialCachedFiltersState,
+    characteristics: [],
+    currentPage: 0,
+    totalPages: 0,
+    isLoading: true,
+    isLoadingChar: false,
+    prices: {
+      max: 0,
+      min: 0,
+    },
+    getCategories: async () => {
+      try {
+        set({ isLoading: true });
+        const data = await getCategoriesRequest();
+        set({ categories: data });
+        return data;
+      } catch {
+        throw new Error();
+      } finally {
+        set({ isLoading: false });
+      }
+    },
+    getProducts: async (filters) => {
+      try {
+        set((state) => {
+          state.isLoading = true;
+          state.currentPage = 1;
+          state.filters = {
+            ...state.filters,
+            ...filters,
+          };
+        });
+        const data = await getProductsRequest(get().filters, get().currentPage);
+        set({
+          totalPages: data.totalPages,
+          products: data.products,
+        });
+        return data.products;
+      } catch {
+        throw new Error();
+      } finally {
+        set({ isLoading: false });
+      }
+    },
+    onEndReached: async () => {
+      try {
+        set((state) => {
+          state.isLoading = true;
+          state.currentPage += 1;
+        });
+        const data = await getProductsRequest(get().filters, get().currentPage);
+        set((state) => {
+          state.totalPages = data.totalPages;
+          state.products = [...state.products, ...data.products];
+        });
+        return data.products;
+      } catch {
+        throw new Error();
+      } finally {
+        set({ isLoading: false });
+      }
+    },
+    refreshProducts: async () => {
+      try {
+        set((state) => {
+          state.isLoading = true;
+          state.currentPage = 1;
+        });
+        const data = await getProductsRequest(get().filters, get().currentPage);
+        set({ totalPages: data.totalPages, products: data.products });
+        return data.products;
+      } catch {
+        throw new Error();
+      } finally {
+        set({ isLoading: false });
+      }
+    },
+    getCharacteristics: async (category) => {
+      try {
+        set({ isLoadingChar: true });
+        const data = await getCharacteristicsRequest(category);
+        set((state) => {
+          state.characteristics = data.characteristics;
+          state.prices = data.prices;
+          state.cachedCategory = category;
+          state.filters = {
+            ...state.filters,
+            max: data.prices.max,
+            min: data.prices.min,
+            category: state.filters.category,
+          };
+        });
+        return data.characteristics;
+      } catch {
+        throw new Error();
+      } finally {
+        set({ isLoadingChar: false });
+      }
+    },
+    reset: () => {
+      set({
+        filters: initialFiltersState,
+        products: [],
+        characteristics: [],
+        cachedFilters: initialCachedFiltersState,
+      });
+    },
+    resetFilters: () => {
+      if (
+        get().filters.characteristics.length > 0 ||
+        get().filters.rating ||
+        get().prices.max != get().filters.max ||
+        get().prices.min != get().filters.min
+      ) {
+        set((state) => {
+          state.cachedFilters = state.filters;
+        });
+      }
+      set((state) => {
+        state.filters = {
           ...state.filters,
-          ...filters,
-        },
-      }));
-      const data = await getProductsRequest(get().filters, get().currentPage);
-      set(() => ({
-        totalPages: data.totalPages,
-        products: data.products,
-      }));
-      return data.products;
-    } catch {
-      throw new Error();
-    } finally {
-      set({ isLoading: false });
-    }
-  },
-  onEndReached: async () => {
-    try {
-      set(() => ({ isLoading: true, currentPage: get().currentPage + 1 }));
-      const data = await getProductsRequest(get().filters, get().currentPage);
-      set((state) => ({
-        totalPages: data.totalPages,
-        products: [...state.products, ...data.products],
-      }));
-      return data.products;
-    } catch {
-      throw new Error();
-    } finally {
-      set({ isLoading: false });
-    }
-  },
-  refreshProducts: async () => {
-    try {
-      set(() => ({ isLoading: true, currentPage: 1 }));
-      const data = await getProductsRequest(get().filters, get().currentPage);
-      set(() => ({ totalPages: data.totalPages, products: data.products }));
-      return data.products;
-    } catch {
-      throw new Error();
-    } finally {
-      set({ isLoading: false });
-    }
-  },
-  getCharacteristics: async (category) => {
-    try {
-      set({ isLoadingChar: true });
-      const data = await getCharacteristicsRequest(category);
-      set((state) => ({
-        characteristics: data.characteristics,
-        prices: data.prices,
-        cachedCategory: category,
-        filters: {
-          ...state.filters,
-          max: data.prices.max,
-          min: data.prices.min,
+          ...initialFiltersState,
           category: state.filters.category,
-        },
-      }));
-      return data.characteristics;
-    } catch {
-      throw new Error();
-    } finally {
-      set({ isLoadingChar: false });
-    }
-  },
-  reset: () => {
-    set(() => ({
-      filters: initialFiltersState,
-      products: [],
-      characteristics: [],
-      cachedFilters: initialCachedFiltersState,
-    }));
-  },
-  resetFilters: () => {
-    if (
-      get().filters.characteristics.length > 0 ||
-      get().filters.rating ||
-      get().prices.max != get().filters.max ||
-      get().prices.min != get().filters.min
-    ) {
-      set({ cachedFilters: get().filters });
-    }
-    set({
-      filters: { ...get().filters, ...initialFiltersState, category: get().filters.category },
-    });
-  },
-  resetCache: () => {
-    set({ cachedFilters: initialCachedFiltersState });
-  },
-}));
+        };
+      });
+    },
+    resetCache: () => {
+      set({ cachedFilters: initialCachedFiltersState });
+    },
+  })),
+);
